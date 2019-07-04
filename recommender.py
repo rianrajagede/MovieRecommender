0ed3baf74b6661cedf23ee86b9e5c6052782d19a
rianrajagede <rian.adam.r@gmail.com>
2019-07-04 15:08:10 +0700

    feat: improved algorithm using average value

--- recommender.py
+++ recommender.py
@@ -2,14 +2,13 @@ import pandas as pd
 import numpy as np
 
 features = ["Comedy","Drama","Action","Romance","Adventure","Thriller","Crime","Fantasy","Children","Animation","Sci-Fi","Horror","Mystery","IMAX","Musical","Documentary","War","Western","(no genres listed)",
-            "Todd Phillips","Dennis Dugan","Peter Farrelly","Jay Roach","Frank Coraci",
-            "Adam Sandler","Will Ferrell","Johnny Depp","Jim Carrey","Vince Vaughn","Ben Stiller","Robert De Niro","Paul Rudd","Jennifer Aniston","Jack Black","Drew Barrymore","Cameron Diaz"]
+            "Adam Sandler","Will Ferrell"]  # top 2 watched movie's actor
 
 data = pd.read_csv("formatted_data/data_train_111.csv")
 datatest = pd.read_csv("formatted_data/data_test_111.csv")
 data["userRating"] = pd.to_numeric(data["userRating"])
 
-# formatting data to become user x movies matrix
+# formatting data to have one hot encoding features
 def reformat(data, features):
     for i,f in enumerate(features):
         data[f] = 0.0
@@ -17,12 +16,9 @@ def reformat(data, features):
             if i < 19:
                 if f in row["genres"]:
                     data.loc[index, f] = 1.0
-            elif i < 23:
-                if f in row["directors"]:
-                    data.loc[index, f] = 0.2
-            elif i < 35:
+            else:
                 if f in row["actors"]:
-                    data.loc[index, f] = 0.3
+                    data.loc[index, f] = 1.0 # change this value to create weighted features
     data = data.drop(["title","userId","imdbRating","genres","directors","actors"],axis=1)
     return data
 
@@ -33,17 +29,24 @@ datatest = reformat(datatest, features)
 for f in features:
     data[f] *= data["userRating"]
 
-data.to_csv("fitur_item.csv", sep=',',encoding='utf-8', index=False)
+# see the result
+data.to_csv("train_matrix.csv", sep=',',encoding='utf-8', index=False)
 
 # Get User Profile
-user_profile = data.loc[:,"Comedy":].sum(axis=0)                     
-user_profile = user_profile / user_profile.sum()
-print(user_profile)                        
+sum_score = data.loc[:,"Comedy":].sum(axis=0)
+sum_ones = data.loc[:,"Comedy":].astype(bool).sum(axis=0) # total non zero value
+user_profile = sum_score / sum_ones
+print(user_profile)
 
 # Scoring
 datatest["value"] = 0.0
+for i in range(len(datatest)):
+    datatest.loc[i,"Comedy":]*=user_profile
+    
 for index, row in datatest.iterrows():
-    datatest.loc[index, "value"]=(datatest.loc[index,"Comedy":]*user_profile).sum(axis=0)
+    test_sum_score = datatest.loc[index,"Comedy":].sum()
+    test_sum_ones = datatest.loc[index,"Comedy":].astype(bool).sum() # total non zero value
+    datatest.loc[index, "value"] = test_sum_score / test_sum_ones
 
 # SAVE
-datatest.loc[:,["userRating", "imdbId","value"]].to_csv("result.csv", sep=',',encoding='utf-8', index=False)
+datatest.loc[:,["imdbId", "userRating","value"]].to_csv("result.csv", sep=',',encoding='utf-8', index=False)
